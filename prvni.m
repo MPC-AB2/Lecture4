42c9a176aef41b91bb72f0a673cdf6ad838e33f6
ondrados <ondrejdoskocil@gmail.com>
2022-03-10 11:32:26 +0100

    update prvni.m

--- prvni.m
+++ prvni.m
@@ -4,8 +4,18 @@ function [depthMaps] = prvni(path)
     for i = 1:length(folders)
         im0 = imread([folders(i).folder, '\', folders(i).name,  '\im0.png']);
         im1 = imread([folders(i).folder, '\', folders(i).name,  '\im1.png']);
+        
+        [vars , vals] = readvars([folders(i).folder, '\', folders(i).name,  '\calib.txt']);
+        doffs = vals(vars=="doffs");
+        baseline = vals(vars=="baseline");
+
+        splited = split(fgetl(fopen([folders(i).folder, '\', folders(i).name,  '\calib.txt'])), '=');
+        A = str2num(splited{2});
+        f = A(1,1);
+
         dispar = disparitySGM(rgb2gray(im0), rgb2gray(im1));
         dispar(isnan(dispar)) = 0;
-        depthMaps{i} = dispar;
+        depthMap = (baseline*f)./(dispar+doffs) ;
+        depthMaps{i} = depthMap;
     end
 end
